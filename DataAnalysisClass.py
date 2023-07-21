7022d8ef41d97338aa156c9ace555ac6d3b6a553
Jack Morse <jack.morse@stfc.ac.uk>
2023-07-21 14:16:19 +0100

    Add file

---
+++ DataAnalysisClass.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 21 13:56:45 2023
+
+@author: Jack Morse
+
+A class to help with general data analysis and manipulation.
+"""
+class DataAnalysis:
+    pass
+
+"""
+Add a method to make a data directory and a file to save data to and make it easy to save data
+"""
+
